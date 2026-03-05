e6de28563722fbc0400f3d74314c35540421a465
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-03-05 18:48:51 -0300

    feat: initial struct on project

---
+++ src/test/java/com/desafio/backend/picpay_simplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.backend.picpay_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
